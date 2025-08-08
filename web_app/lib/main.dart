a9b0373ea269249b20697bd5324c8adfd6c38f48
koushikadak07 <koushikadakka2004@gmail.com>
2025-08-09 01:39:57 +0530

    fixed

--- web_app/lib/main.dart
+++ web_app/lib/main.dart
@@ -1,3 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp()
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: SafeArea(
+//           child: WebViewWidget(
+//             controller: WebViewController()
+//               ..setJavaScriptMode(JavaScriptMode.unrestricted)
+//               // ..loadRequest(Uri.parse("https://figma-land-six.vercel.app/")),
+//               ..loadRequest(Uri.parse("https://encrypt-share-ochre.vercel.app/")),
+//
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -10,7 +31,8 @@ void main() {
           child: WebViewWidget(
             controller: WebViewController()
               ..setJavaScriptMode(JavaScriptMode.unrestricted)
-              ..loadRequest(Uri.parse("https://thehub-xi.vercel.app/")),
+            // ..loadRequest(Uri.parse("https://figma-land-six.vercel.app/")),
+              ..loadRequest(Uri.parse("https://weatherupdatebysp.vercel.app/")),
           ),
         ),
       ),
